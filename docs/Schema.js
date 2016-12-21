1a5a10b3021086dd039832b727cf8b635337b524
Hien Tran <aznketchup@yahoo.com>
2016-12-21 14:28:57 -0800

    Write out how tables are going to look like.
    -Change name of database diagram.

---
+++ docs/Schema.js
@@ -0,0 +1,24 @@
+/*
+users
+  *username(PK)
+  *password
+  *first name
+  *last name
+
+personalExercise
+  *uniqueID(PK)
+  *username
+  *exercise
+
+exercises
+  *exercise
+
+muscles
+  *muscles
+
+targetMuscles
+  *uniqueID
+  *exercise
+  *muscles
+
+*/
